db825ea357d64926e4144d103a86e5fbb60e0427
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-02-27 09:39:14 -0700

    Updates 0.0.3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,10 @@ class PagesController < ApplicationController
     @page = Page.new
   end
 
+  def edit 
+    @page = Page.find(params[:id])
+  end 
+
   def create
     @page = Page.new(page_params)
 
@@ -19,6 +23,21 @@ class PagesController < ApplicationController
       else
         render :new
       end
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
   end
 
   private
